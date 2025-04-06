e4d69d20784566ad3e1c2a92681edd8ffc3afb0d
usmanBoi <usmankith@gmail.com>
2025-04-06 23:40:55 +0500

    base need changes

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -1,21 +1,19 @@
 import { FC } from "react";
-import { Content, ImageField } from "@prismicio/client";
+import { Content } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
-import { PrismicNextImage } from "@prismicio/next";
+// import { PrismicNextImage } from "@prismicio/next";
 
-
-
-type HeroImgProps = {
-	image: ImageField;
-	className?: string;
-};
+// type HeroImgProps = {
+//   image: ImageField;
+//   className?: string;
+// };
 /**
  * Props for `Hero`.
  */
 export type HeroProps = SliceComponentProps<Content.HeroSlice>;
 
 /**
- * Component for "Hero" Slices.
+ * Component for "Hero" Slicesa.
  */
 const Hero: FC<HeroProps> = ({ slice }) => (
   <section
@@ -24,13 +22,14 @@ const Hero: FC<HeroProps> = ({ slice }) => (
     className="border-2 border-red-500"
   >
     <div>
-      <PrismicNextImage
+      {/* <PrismicNextImage
         priority
         field={}
         className="h-full w-full object-cover object-center"
-      />
+      /> */}
     </div>
     <h1 className="font-extrabold">{slice.primary.heading}</h1>
+    <h2>{slice.primary.tagline}</h2>
     <h2>{slice.primary.tagline}</h2>
     <div className="flex items-center justify-center gap-4">
       {slice.primary.cta_buttons.map((item, i) => (
